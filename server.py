29ef9092780fb0242c8ad3cd1edc49a288550d31
adamhwu <wuadam42@gmail.com>
2025-04-12 22:29:17 -0700

    Initial render-ready socket server

---
+++ server.py
@@ -0,0 +1,44 @@
+# server.py
+import asyncio
+from websockets.asyncio.server import broadcast
+import websockets
+
+connected_clients = set() 
+
+async def handler(websocket):
+    connected_clients.add(websocket)
+    package = {}
+
+    try:
+        async for message in websocket:
+            # data = json.loads(message)
+            # if isinstance(data, str):
+            #     data = json.loads(data)
+            #
+            # if data["type"] == "sensor":
+            #     package = {
+            #         "type": "sensor", 
+            #         "temperature": data["temperature"],
+            #         "humidity": data["humidity"],
+            #         "pulse": data["pulse"]
+            #     }
+            #     json_msg = json.dumps(package)
+            #
+            # else:
+            #     json_msg = json.dumps(data)
+            #
+            broadcast(connected_clients, message)
+
+    except websockets.ConnectionClosed:
+        print("Client disconnected")
+
+    finally:
+        connected_clients.remove(websocket)
+
+
+async def main():
+    async with websockets.serve(handler, "", 10000):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
